6d3cf5e3de8db9d64316f4360d7363052f5d2e11
vitalii <dumavb777@gmail.com>
2019-05-28 13:17:50 +0300

    sol

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ function Person(name, date, amount) {
         this.amount += sum;
         const transaction = `${title}: ${sum}`;
         transactions = [...transactions, transaction];
-
     };
 
     this.withdrawMoney = function(sum, title) {
@@ -30,15 +29,3 @@ function Person(name, date, amount) {
         console.log(transactions);
     }
 }
-
-const dmytro = new Person('Dmytro', '11.26.1994', 1000);
-const pavel = new Person('Pavel', '06.06.1990', 400);
-
-dmytro.getInfo(); // print `Name: Dmytro, Age: 24, Amount: 1000$`
-dmytro.addMoney(2000, 'salary');
-dmytro.withdrawMoney(500, 'new phone');
-dmytro.getInfo(); // Name: Dmytro, Age: 24, Amount: 2500$
-dmytro.withdrawMoney(500, 'apartment rent');
-dmytro.getAccountHistory(); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
-
-pavel.getInfo(); // // Name: Pavel, Age: 28, Amount: 400$
