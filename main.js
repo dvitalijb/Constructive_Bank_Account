03d6804621eb40621d4ae1e6cce2496f3ce37ee9
vitalii <dumavb777@gmail.com>
2019-06-06 13:57:32 +0300

    sol

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 function Person(name, date, amount) {
-    this.transactions = [];
     this.name = name;
-    this.date = date;
+    this.date = new Date (date.split('.').reverse());
     this.amount = amount;
-    this.transactions = [this.transactions, `Initial: ${this.amount}`];
+    this.transactions = [];
+    this.transactions.push(`Initial: ${this.amount}`);
 }
 
 Person.prototype.getInfo = function() {
-    const today = new Date();
-    const birthDate = new Date(this.date);
-    this.age = today.getFullYear() - birthDate.getFullYear();
+    this.age =  Math.floor((Date.now()-Date.parse(this.date))/(60*60*24*366*1000));
     console.log(`Name: ${this.name}, Age: ${this.age}, Amount: ${this.amount}$`);
 };
 
@@ -28,3 +26,15 @@ Person.prototype.withdrawMoney = function(sum, title) {
 Person.prototype.getAccountHistory = function() {
     console.log(this.transactions);
 };
+
+const dmytro = new Person('Dmytro', '26.11.1994', 1000);
+const pavel = new Person('Pavel', '06.06.1990', 400);
+
+console.log(dmytro.getInfo()); // print `Name: Dmytro, Age: 24, Amount: 1000$`
+console.log(dmytro.addMoney(2000, 'salary'));
+console.log(dmytro.withdrawMoney(500, 'new phone'));
+console.log(dmytro.getInfo()); // Name: Dmytro, Age: 24, Amount: 2500$
+console.log(dmytro.withdrawMoney(500, 'apartment rent'));
+console.log(dmytro.getAccountHistory()); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
+
+console.log(pavel.getInfo()); // // Name: Pavel, Age: 28, Amount: 400$
